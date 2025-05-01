2da7fe8add0bfeead04db1633eb16252554b7059
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-04-30 21:25:52 -0700

    uploadFiles
    Actualizando el proyecto en Git

---
+++ First-Practice/06_diccionary.py
@@ -0,0 +1,10 @@
+#Un diccionario es una estructura de datos que almacena pares de clave-valor
+diccionario_datos = {'clave' : 'valor'}
+
+data_diccionary = {'nombre': 'Victor Hernandez', 'Edad':45, 'Ciudad': 'Cojedes'}
+
+# Acceder a los valores mediante claves
+# Access values via key.
+print(data_diccionary['nombre'])
+
+#Agregar Elementos
