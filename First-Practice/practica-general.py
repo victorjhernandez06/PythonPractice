49edd802a373f6079c63d14b0baf27b1c8984104
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-07-02 15:05:54 -0700

    Nuevo_27

---
+++ First-Practice/practica-general.py
@@ -0,0 +1,44 @@
+#Numbers
+
+p1 = 2
+p2 = 2
+print(p1 + p2)
+
+print(50 - 5 * 6)
+
+print( (50 - 5*6) / 4)
+
+print(8 / 5) #division always returns a floating-point number
+
+print(17 /3) #classic division returns a float
+
+print(17 // 3) #floor division discards the fractional part
+
+print(17 % 3) # the operator returns the remainder of the division
+
+print(5*3+2) # floored quotient * divisor + remainder
+
+print(5 ** 2) # squared
+
+print(2 ** 7) # 2 to the power of 7
+
+width = 20
+height = 5 * 9
+print(width * height)
+
+tax = 12.5 / 100
+price = 100.50
+print(price * tax)
+
+#text
+
+print('spam eggs')
+print ('Doesn\'t') # use \' to escape yhe single quote
+print("Doesn't") #...or use double quotes instead
+
+print ('"Yes," they said.')
+print("\"Yes,\" they said.")
+print('"Ins\'t," "they said')
+
+s = 'First line.\nSecond Line.' # \n means newline
+print (s)
