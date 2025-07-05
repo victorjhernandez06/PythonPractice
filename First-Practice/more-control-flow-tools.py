5ea163bbc6022d2b67f873141c19ddd0e09e324c
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-07-04 21:13:31 -0700

    03072025:More

--- First-Practice/more-control-flow-tools.py
+++ First-Practice/more-control-flow-tools.py
@@ -123,9 +123,28 @@ for num in range(2, 10):
 # Found an even number8
 # Found an odd number9
 
+print('ELSE CLAUSES ON LOOPS')
 '''ELSE CLAUSES ON LOOPS'''
 # in a for or while loop the break statement may be paired with an else clause. If the loop finishes without executing the break, the else clause executes.
-# in a for loop the else clause is execute after the loop finishes its final iteraction, if no break ocurred.
+# in a for loop the else clause is executing after the loop finishes its final interaction, if no break occurred.
+# in a while loop, it's executed after the loop's condition become false.
+# In either kind of loop, the else clause is not executed if the loop was terminated by a break. Of course, other ways of ending the loop early such as a return or a raised exception, will also skip execution of the else clause.
+# this is exemplified in the folllowing for loop, which searches for prime numbers:
 
-
+for n in range(2,10):
+    for x in range(2,n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
+# 2 is a prime number
+# 3 is a prime number
+# 4 equals 2 * 2
+# 5 is a prime number
+# 6 equals 2 * 3
+# 7 is a prime number
+# 8 equals 2 * 4
+# 9 equals 3 * 3
 
