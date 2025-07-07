5a6e75d19d292cee0c0ac96cb409da50be6346b9
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-07-06 23:15:46 -0700

    06072025_more

--- First-Practice/more-control-flow-tools.py
+++ First-Practice/more-control-flow-tools.py
@@ -266,5 +266,32 @@ ejes = Point(0,0)
 print(ejes) #--> Point(0, 0)
 Point.where_is(ejes) #--> Origin
 
-#
+'''DEFINING FUNCTIONS'''
+
+def fib(n):
+    """Print a fibonacci series less than n."""
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b =b, a+b
+    print()
+#now call the function we just defined:
+fib(2000) #--> 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597
+
+f = fib
+f(100) #--> 0 1 1 2 3 5 8 13 21 34 55 89
+print(f)
+
+fib(0)
+print(fib(0)) #--> None
+
+def fib2(n):
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+f100 = fib2(200) #call it
+print(f100) # --> [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
 
