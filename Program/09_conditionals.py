ccb12eda3575e3e036ac39e3a2223439cef64595
Victor Hernandez <94196362+victorjhernandez06@users.noreply.github.com>
2025-07-10 21:46:10 +0000

    wordspace

--- Program/09_conditionals.py
+++ Program/09_conditionals.py
@@ -5,5 +5,6 @@
 my_conditions =  True
 
 if my_conditions == True:
-    print("la condicion se ejecuta")
+    print("la condicion se ejecuta la condicion del if")
+
 print("la condicion no se ejecuta")
