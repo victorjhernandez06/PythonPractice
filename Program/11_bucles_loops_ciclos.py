d559e261299d4de435d058122c4a3e839a8a57e1
victordev06 <victordev06@Victordev06.victordev06>
2025-07-16 14:07:53 -0700

    pass_sentence

--- Program/11_bucles_loops_ciclos.py
+++ Program/11_bucles_loops_ciclos.py
@@ -80,11 +80,72 @@ else:
 # i is no longer less than 6
 print('continue with more exercise from while loops...')
 
+
+
 """Python for loops"""
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set or a string)
 # this is less like for keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.
 # With he for loop we can execute a set statements, once for each item in a list, tuple, set, etc. 
 
+# Exercise Brais.
+print('////---////')
+
+my_list = [35, 24, 62, 52, 30, 30, 17]
+for element in my_list:
+    print(element)
+# 35
+# 24
+# 62
+# 52
+# 30
+# 30
+# 17
+
+my_tuple  = (35, 1.44, 'victor','Hernandez','Adrian')
+for element in my_tuple:
+    print(element)
+# 35
+# 1.44
+# victor
+# Hernandez
+# Adrian
+
+setup_2025 = ['Linux Debian','Linux Ubuntu',"Windows 11"]
+for myself in setup_2025:
+    print(myself)
+# Linux Debian
+# Linux Ubuntu
+# Windows 11
+
+my_set = {'Karina', 'Mathias',10}
+for element in my_set:
+    print(element)
+# 10
+# Karina
+# Mathias
+
+my_dict = {'Nombre': 'Sofia', 'Apellido':'Hernandez', 'Edad':9}
+for element in my_dict:
+    print(element)
+else:
+    print("el bucle for ha finalizado")
+# Nombre
+# Apellido
+# Edad
+# el bucle for ha finalizado
+
+# Si queremos iterar los valores dentro de un diccionario, debemos ponerlo en una lista, puesto que al iterar con el for, en un principio solo obtendremos las keys. 
+# list(my_dict.values())
+my_dict = {'Nombre': 'Sofia', 'Apellido':'Hernandez', 'Edad':9}
+for element in list(my_dict.values()): #
+    print(element)
+# Sofia
+# Hernandez
+# 9
+
+
+
+print('////---////')    
 # Example
 # print each fruit in a fruit list
 objects = ['set_1','set_2','set_3']
@@ -184,4 +245,25 @@ else:
 # 4
 # 5
 # finally finished
+ 
+# Break the loop when x is 3, and see what happens with the else block.
+# Aqui el bloque se detiene por el break cuando 3 == 3, entonces, el else no se ejecuta. 
+for x in range(6):
+    if x == 3: break
+    print(x)
+else:  
+    print("finally finished1") 
+# 0
+# 1
+# 2
 
+"""Nested  lopps"""
+# A nested loops is lopp inside a loop
+# The "inner loop" will be executed one time for each iteration of the "outer loop"
+
+
+"""the pass statement"""
+# for the loops cannot be empty, but if you for some reason have a for loop with not content, put in the pass statement to avoid getting an error.
+
+for x in [0,1,2]:
+    pass #aqui no pasa nada, el bucle solo queda en el aire, esperando a que algun dia le des utilidad, como esta vacio, para no generar error le colocas la palabra reservada pass.
---
+++ Program/12_sentence_pass.py
@@ -0,0 +1,15 @@
+# Programa principal
+from math import factorial
+
+# Definicion de funciones:
+def factorial(num):
+    #pendiente
+
+# Script para encontrar el factorial
+# Programa Principal.
+numero  = int(input("Ingrese un numero natural: "))
+if numero >= 0:
+    el_factorial = factorial(numero)
+    print(f"el factorial de: {numero} es {el_factorial}")
+else: 
+    print("no existe el factorial de un numero negativo.")
