9fc6b83433bf5ce1e5b3589fbe77665eb56850ce
victordev06 <victordev06@Victordev06.victordev06>
2025-07-16 22:00:37 -0700

    debian_07_15_25

--- Program/13_functions.py
+++ Program/13_functions.py
@@ -1,65 +1,134 @@
+
 ###FUNCTIONS###
+# A function is a block of code wich only runs when it is called.
+# You can pass data, know as parameters, into a function.
+# A function can return data as a result.
+
+"""Create a function"""
+# in python a function is defined using a def keyword.
+# Example
+import datetime
 
 
-
-
-# Exercises (simple)
 def my_function():
-    print('this is a function')
+    print('A new function example')
     
-my_function() #Esta es la llamada a la funcion, con esto aparece en pantalla: 'esto es una funcion'
+"""Call a function"""
+# to call a function, use the function name followed by parenthesis
+my_function()
+
+"""Arguments"""
+# information can be passed into functions as arguments.
+# Arguments are specified after the function name, inside the parentheses. You can add as many arguments as you want, just separate them with a comma.
+# The following example has a fucntion with one argument (fname). When the function is called, we pass along a first name, which is used inside the function to print the full name:
+
+def my_function(fname):
+    print(fname + " Hernandez")
+  
+my_function('Victor') #--> Victor Hernandez
+# my_function('Karina')
+my_function('Adrian') #--> Adrian Hernandez
+my_function('Mathias') #--> Mathias Hernandez
+my_function('Sofia') #--> Sofia Hernandez  
 
 
-def sum_two_values (first_num,  second_num):
-    print(first_num + second_num) #hace una suma entre dos parametros.
+""" Parameters or Arguments """
+
+
+
     
-sum_two_values(23, 21) #ejecuta la funcion e imprime la suma dentro de la funcion.
-
-def sum_a_b (a, b):
-    return(a+b)
-
-print(sum_a_b(2,2)) #  --> devuelve e imprime la suma de 2+2 = 4
-print(sum_a_b(1.4,22.8)) # --> 24.2 
-
-
-# Al tipado no funciona aqui.
-# Aqui he tipado el numero, pero al indicar que son string los datos, no toma en cuenta este tipado. NO sirve de nada por el tiempo de ejecucion, pues tomara los datos que le metamos datos. 
-def function_2(numb1:int, numb2:int): #aqui probamos tipando los parametros
-    return(numb1+numb2)
-print(function_2('22','20'))
-
-###
-def sum_two_val (a, b):
-    return(a + b)
-
-my_result = sum_two_val(2,3)
-print(my_result) #-->5
-
-### Usando el retorn 
-def sum1(a,b):
-    my_sum = a + b
-    return my_sum
-
-operation = sum1(2,32)
-print(operation) #--> 34
-
-def print_name(name, surname):
-    print(f"{name} {surname}")
-
-print_name('Victor','Hernandez') #--> Victor Hernandez
-print_name(surname="Hernandez",name='Victor') #aqui reordene el nombre, indicando el valor de los parametros en la llamada. ##--> Victor Hernandez
-
-
-### Aqui utilizamos parametros definidos dentro de la funcion, sin usar el constructor.
-def print_name_with_default(name,surname, alias = 'Sin Alias'):
-    print(f"{name} {surname} {alias}")
     
-print_name_with_default('Victor','Hernandez','Victordev06') #--> Victor Hernandez Victordev06
-print_name_with_default('Victor','Hernandez') #--> Victor Hernandez Sin Alias
-
-### el asterico indica que puedo incluir muchos parametros del mismo tipo
-def print_texts(*text):
-    print(text)
     
-print_texts('Hola')
+    
+    
+    
+    
+# # Exercises (simple)
+# def my_function():
+#     print('this is a function')
+    
+# my_function() #Esta es la llamada a la funcion, con esto aparece en pantalla: 'esto es una funcion'
+
+
+# def sum_two_values (first_num,  second_num):
+#     print(first_num + second_num) #hace una suma entre dos parametros.
+    
+# sum_two_values(23, 21) #ejecuta la funcion e imprime la suma dentro de la funcion.
+
+# def sum_a_b (a, b):
+#     return(a+b)
+
+# print(sum_a_b(2,2)) #  --> devuelve e imprime la suma de 2+2 = 4
+# print(sum_a_b(1.4,22.8)) # --> 24.2 
+
+
+# # Al tipado no funciona aqui.
+# # Aqui he tipado el numero, pero al indicar que son string los datos, no toma en cuenta este tipado. NO sirve de nada por el tiempo de ejecucion, pues tomara los datos que le metamos datos. 
+# def function_2(numb1:int, numb2:int): #aqui probamos tipando los parametros
+#     return(numb1+numb2)
+# print(function_2('22','20'))
+
+# ###
+# def sum_two_val (a, b):
+#     return(a + b)
+
+# my_result = sum_two_val(2,3)
+# print(my_result) #-->5
+
+# ### Usando el retorn 
+# def sum1(a,b):
+#     my_sum = a + b
+#     return my_sum
+
+# operation = sum1(2,32)
+# print(operation) #--> 34
+
+# def print_name(name, surname):
+#     print(f"{name} {surname}")
+
+# print_name('Victor','Hernandez') #--> Victor Hernandez
+# print_name(surname="Hernandez",name='Victor') #aqui reordene el nombre, indicando el valor de los parametros en la llamada. ##--> Victor Hernandez
+
+
+# ### Aqui utilizamos parametros definidos dentro de la funcion, sin usar el constructor.
+# def print_name_with_default(name,surname, alias = 'Sin Alias'):
+#     print(f"{name} {surname} {alias}")
+    
+# print_name_with_default('Victor','Hernandez','Victordev06') #--> Victor Hernandez Victordev06
+# print_name_with_default('Victor','Hernandez') #--> Victor Hernandez Sin Alias
+
+# ### el asterico indica que puedo incluir muchos parametros del mismo tipo
+# def print_texts(*text):
+#     print(text)
+    
+# print_texts('Hola')# --> ('Hola',)
+
+
+# ### El asteristo * indica que es una funcion con parametros arbitrarios
+# def print_texts(*texts):
+#     for text in texts:
+#         print(text.upper())
+
+# print_texts('Victor','Karina','Adrian','Mathias','Sofia')
+# # VICTOR
+# # KARINA
+# # ADRIAN
+# # MATHIAS
+# # SOFIA
+
+# """MICROSOFT PYTHON """
+
+# def print_time():
+#     print('task completed')
+#     print(datetime.datetime.now())
+#     print()
+
+# first_name  = 'Victor'
+# print_time()
+
+# for x in range(0,10):
+#     print(x)
+
+ 
+
 
